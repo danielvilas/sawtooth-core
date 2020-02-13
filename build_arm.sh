f03b9efc2bbac32be4d69beeda7c5c710e084cb8
Daniel Vilas Perulan <dvilaspe@everis.com>
2020-02-13 13:45:03 +0100

    update

--- build_arm.sh
+++ build_arm.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 docker build . -f validator/Dockerfile.arm -t sawtooth-core-base-arm:1.2.4
-#docker build . -f validator/Dockerfile.arm -t danielvilas/sawtooth-validator-arm:1.2.4
+docker build . -f validator/Dockerfile.arm -t danielvilas/sawtooth-validator-arm:1.2.4
 docker build . -f adm/Dockerfile.arm -t danielvilas/sawtooth-adm-arm:1.2.4
 #docker build . -f families/settings/Dockerfile.arm -t danielvilas/sawtooth-settings-tp-arm:1.2.4
 #docker build . -f families/smallbank/smallbank_rust/Dockerfile.arm -t danielvilas/smallbank-rust-tp-arm:1.2.4
